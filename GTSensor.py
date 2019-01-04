69b7136c90d2bb0095f9318822168b7c04092a87
Franklin Embate <ferox.dragon@gmail.com>
2019-01-04 10:38:29 +0800

    yeah

--- GTSensor.py
+++ GTSensor.py
@@ -176,6 +176,7 @@ class GTSensor:
 			raise RuntimeError("Couldn't send packet.")
 
 	def enrollmentThird(self):
+		print("im in third")
 		if self.writePacket(GT521F5.THIRD_MATCH_SAVE.value, GT521F5.DEFAULT.value):
 			return self.receivedPacket()
 		else:
