d648ae943e060710a57b87c4da4f379afc82cbef
Franklin Embate <ferox.dragon@gmail.com>
2019-01-04 17:31:40 +0800

    yeah

--- GTSensor.py
+++ GTSensor.py
@@ -27,6 +27,7 @@ class GTSensor:
 			exit(1)
 
 	def writePacket(self, cmd, param):
+		print(bytearray(struct.pack(GT521F5.COMM_STRUCT(), 0x55, 0xAA, self.address, 1, 64)))
 		packet = bytearray(struct.pack(GT521F5.COMM_STRUCT(), 0x55, 0xAA, self.address, param, cmd))
 		checksum = sum(packet)
 		packet += bytearray(struct.pack(GT521F5.CHECK_SUM_STRUCT(), checksum))
