fcd779014e511c14b9ca9dfc197623e754cd88fb
Franklin Embate <ferox.dragon@gmail.com>
2019-01-25 16:26:32 +0800

    yeah

--- GTMainV2.py
+++ GTMainV2.py
@@ -62,7 +62,7 @@ class App:
 	def __capture_the_lights__(self):
 	        if self.sensor.senseFinger()[0]['Parameter'] == 0:
 	                print ("Capturing Fingerprint")
-	                sleep(1)
+	                sleep(0.5)
 	                if self.sensor.captureFinger(True)['ACK']:
 	                        print ("Captured")
 	                        self.sensor.LED(False)
