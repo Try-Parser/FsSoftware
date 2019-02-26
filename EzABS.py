22660d33a21a46d22bfe3bd034669744d1de8e88
Franklin Embate <ferox.dragon@gmail.com>
2019-02-26 11:37:45 +0800

    v2-update

--- EzABS.py
+++ EzABS.py
@@ -69,7 +69,7 @@ class EzABS:
 					else:
 						success = False
 						code = 906
-					preparedPayload = '{ "command": "SENSOR_STATUS", "mac_address": "'+ str(hex(uuid.getnode()))+'", "success": "' +str(success)+'", "message": "'+ response +'", "code": "'+ str(code) +'", "scannerId": "'+ content["scannerId"] +'", "id": "' + content["uid"] + '", "empId": "' + content["emp"]["uid"] + '" }'
+					preparedPayload = '{ "command": "SENSOR_STATUS", "mac_address": "'+ str(hex(uuid.getnode()))+'", "success": "' +str(success)+'", "message": "'+ response +'", "code": "'+ str(code) +'", "scannerId": "'+ str(content["scannerId"]) +'", "id": "' + str(content["uid"]) + '", "empId": "' + str(content["emp"]["uid"]) + '" }'
 					self.ws.send(preparedPayload)
 		else:
 			print("WTF")
